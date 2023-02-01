2fbed18a0dec039dc6b1547df4d7080364856fd1
Diego Hernandez <diegoalejandro061292@gmail.com>
2023-01-31 22:26:11 -0500

    arreglos

--- primero/arreglos/script.js
+++ primero/arreglos/script.js
@@ -5,8 +5,27 @@ console.log(palabaraUsuario);
 const arregloPalabra = palabaraUsuario.split(",");
 console.log(arregloPalabra);
 
+//Crear un array vacío, luego generar 10 números al azar y guardarlos en un array. Mostrar en consola el resultado del array.
+// let min = 1;
+// let max = 10;
+// let ajajaja = Math.floor(Math.random()*(max - min)+min);
+// console.log(ajajaja);
+
+function aleatorio(){
+const arry=[];
+for (let i=0; i<10; i++){
+  arry.push(Math.floor(Math.random()*100));
+}
+return arry;
+}
 
 
 
+// function aleatorio(min, max){
+//     return Math.floor(Math.random() * (max - min) + min);
+//   }
 
-
+//   for (let i =1; i<=10;++i){
+//     console.log(aleatorio(1,4));
+//   }
+  
